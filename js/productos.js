1445223527486569b2e3108a9af452cb1689c6ad
KAESZAR <121060412+KAESZAR@users.noreply.github.com>
2023-12-04 15:53:02 -0300

    Proyecto final Web Peluqueria PP2

---
+++ js/productos.js
@@ -0,0 +1,232 @@
+[ 
+
+    {
+    id: "protec-01",
+    imagen:"img.png/productos.img/oroargan.jpg",
+    titulo:"ORO ARGAN",
+        categoria: {
+        nombre:"Protectores Termicos ", 
+        id:"Protectores Termicos"
+        },
+        precio: 2000
+    },
+   
+    {
+        id: "protec-02",
+        imagen:"img.png/productos.img/biotina1.jpg",
+        titulo:"BIOTINA",
+        categoria: {
+            nombre:"Protectores Termicos",
+            id:"Protectores Termicos"
+        },
+        precio: 2100
+    },
+     
+    {
+        id: "sham-01",
+        imagen:"img.png/productos.img/shampoontritivo.jpg",
+        titulo:"NUTRITIVO",
+        categoria: {
+            nombre:"Shampoo",
+            id: "Shampoo"
+        },
+        precio: 2000
+    }, 
+
+    {
+        id: "aco-01",
+        imagen:"img.png/productos.img/postalisado1.jpg",
+        titulo:"POST ALISADO",
+        categoria: {
+            nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2100
+    }, 
+    
+    {
+        id: "sham-02",
+        imagen:"img.png/productos.img/oroargan5.jpg",
+        titulo:"ORO ARGAN",
+        categoria: {
+            nombre:"Shampoo",
+            id: "Shampoo"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "aco-02",
+        imagen:"img.png/productos.img/bioacond.jpg",
+        titulo:"BIOTINA",
+        categoria: {
+            nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "aco-03",
+        imagen:"img.png/productos.img/premium7.jpg",
+        titulo:"PREMIUM",
+        categoria: {
+            nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "aco-04",
+        imagen:"img.png/productos.img/nutricion8.jpg",
+        titulo:"NUTRICION",
+        categoria: {
+            nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "sham-03",
+        imagen:"img.png/productos.img/kera9.jpg",
+        titulo:"KERTINA",
+        categoria: {
+            nombre:"Shampoo",
+            id: "Shampoo"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "sham-04",
+        imagen:"img.png/productos.img/oroargan5.jpg",
+        titulo:"ORO LIQUIDO",
+        categoria: {
+            nombre:"Shampoo",
+            id: "Shampoo"
+        },
+        precio: 200
+    }, 
+    
+    {
+        id: "protec-03",
+        imagen:"img.png/productos.img/difcons.jpg",
+        titulo:"DIFICIL DE CONSEGUIR",
+        categoria: {
+            nombre:"Protectores Termicos",
+            id:"Protectores Termicos"
+        },
+        precio: 1500
+    }, 
+    
+    {
+        id: "protec-04",
+        imagen:"img.png/productos.img/afterparty.jpg",
+        titulo:"AFTER PARTY",
+        categoria: {
+            nombre:"Protectores Termicos",
+            id: "Protectores Termicos"
+        },
+        precio: 1500
+    }, 
+    
+    {
+        id: "protec-05",
+        imagen:"img.png/productos.img/biotina6.jpg",
+        titulo:"BIOTINA",
+        categoria: {
+            nombre:"Protectores Termicos",
+            id: "Protectores Termicos"
+        },
+        precio: 1800
+    }, 
+    
+    {
+        id: "sham-05",
+        imagen:"img.png/productos.img/oroliq10.jpg",
+        titulo:"ORO ARGAN",
+        categoria: {
+            nombre:"Shampoo",
+            id: "Shampoo"
+        },
+        precio: 2000
+    },
+    
+    {
+        id: "sham-06",
+        imagen:"img.png/productos.img/shanutri.jpg",
+        titulo:"BIOTINA",
+        categoria: {
+            nombre:"Shampoo",
+            id: "Shampoo"
+        },
+        precio: 2100
+    }, 
+    
+    {
+        id: "aco-05",
+        imagen:"img.png/productos.img/nutricion8.jpg",
+        titulo:"NUTRITIVO",
+        categoria: {
+            nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "aco-06",
+        imagen:"img.png/productos.img/postalisado1.jpg",
+        titulo:"POST ALISADO",
+        categoria: {
+        nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2100
+    },
+    
+    {
+        id: "sham-07",
+        imagen:"img.png/productos.img/oroliq10.jpg",
+        titulo:"ORO ARGAN",
+        categoria: {
+            nombre:"Shampoo",
+            id: "Shampoo"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "protec-06",
+        imagen:"img.png/productos.img/biotina6.jpg",
+        titulo:"BIOTINA",
+        categoria: {
+            nombre:"Protectores Termicos",
+            id: "Protectores Termicos"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "aco-07",
+        imagen:"img.png/productos.img/premium7.jpg",
+        titulo:"PREMIUM",
+        categoria: {
+            nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2000
+    }, 
+    
+    {
+        id: "aco-08",
+        imagen:"img.png/productos.img/Acondicionadores.jpg/nutricion8.jpg",
+        titulo:"NUTRICION",
+        categoria: {
+            nombre:"Acondicionadores",
+            id: "Acondicionadores"
+        },
+        precio: 2000
+    }]
